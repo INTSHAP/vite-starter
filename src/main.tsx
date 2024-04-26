e4b64f4e81da577a03f38bcaecc1285c4d23f183
Belloshehu <belloshehu1@gmail.com>
2024-04-26 01:18:57 +0100

    Update App root with context providers

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,22 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 import AuthProvider from "./context/auth-context.tsx";
+import AxiosProvider from "./context/axios-context.tsx";
+import DrawerProvider from "./context/nav-context.tsx";
 
 const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <AuthProvider>
-        <Toaster richColors position="top-center" />
-        <App />
-      </AuthProvider>
+      <DrawerProvider>
+        <AuthProvider>
+          <AxiosProvider>
+            <Toaster richColors position="top-center" />
+            <App />
+          </AxiosProvider>
+        </AuthProvider>
+      </DrawerProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
